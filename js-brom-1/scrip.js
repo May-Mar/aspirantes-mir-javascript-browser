ee29a43ef77ced7db16c962b68cc074d138bfc44
Mayra <mayra.alejandra.mar94@gmail.com>
2023-05-28 23:37:41 -0500

    solucion a los ejercicios de js en el navegador

---
+++ js-brom-1/scrip.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#texto');
+const resultado = document.getElementById('resultado');
+input.addEventListener('input', function(){
+    const text = input.value.toUpperCase();
+    resultado.textContent = text;
+});
+
+const boton = document.getElementsByClassName('btn')[0]; 
+    boton.addEventListener('Click', function(){
+    const text = input.value.toUpperCase();
+    resultado.textContent = text;
+    
+});
+
+
+
